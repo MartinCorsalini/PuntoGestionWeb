18c0447a184aaf8f54aed443f9784cca3b92ce89
MartinCorsalini <martincorsalini@outlook.com>
2025-10-16 20:48:25 -0300

    comenzando

---
+++ src/app/components/certificado-tango/certificado-tango.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificado-tango',
+  imports: [],
+  templateUrl: './certificado-tango.component.html',
+  styleUrl: './certificado-tango.component.css'
+})
+export class CertificadoTangoComponent {
+
+}
---
+++ src/app/pages/inicio-page/inicio-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-page',
+  imports: [],
+  templateUrl: './inicio-page.component.html',
+  styleUrl: './inicio-page.component.css'
+})
+export class InicioPageComponent {
+
+}
