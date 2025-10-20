550f7c6bc19143a5b311123e1929d866f8460a52
MartinCorsalini <martincorsalini@outlook.com>
2025-10-20 20:48:30 -0300

    agregando componentes

--- src/app/pages/inicio-page/inicio-page.component.ts
+++ src/app/pages/inicio-page/inicio-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CarruselComponent } from "../../components/carrusel/carrusel.component";
+import { CertificadoTangoComponent } from "../../components/certificado-tango/certificado-tango.component";
 
 @Component({
   selector: 'app-inicio-page',
-  imports: [],
+  imports: [CarruselComponent, CertificadoTangoComponent],
   templateUrl: './inicio-page.component.html',
   styleUrl: './inicio-page.component.css'
 })
