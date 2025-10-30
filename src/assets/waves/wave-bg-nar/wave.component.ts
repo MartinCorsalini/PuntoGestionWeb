c45e98d1c8c8462b57161567407789f7606fb319
Micaela Prieto <Mprieto@punto-gestion.local>
2025-10-30 14:08:34 -0300

    Background gome

---
+++ src/assets/waves/wave-bg-nar/wave.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wave-bg-nar',
+  imports: [],
+  templateUrl: './wave-bg-nar.component.html',
+  styleUrl: './wave-bg-nar.component.css'
+})
+export class WaveNaranajaComponent {
+
+}
